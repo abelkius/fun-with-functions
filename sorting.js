612cab8b8d5415a461e103824ed83cac9c464bf5
abelkius <abelkius@gmail.com>
2017-10-07 22:49:21 +0200

    sorting algorithms implementations

---
+++ sorting.js
@@ -0,0 +1,45 @@
+
+function mergeSort(arr) {
+  if (arr.length === 1) {
+   return arr;
+  }
+  var length = arr.length;
+  var middle = Math.floor(length / 2);
+  var left = arr.slice(0, middle);
+  var right = arr.slice(middle, length);
+
+  return stich(mergeSort(left), mergeSort(right));
+}
+
+function stich(left, right) {
+  const result = [];
+  while(left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  while(left.length) {
+    result.push(left.shift());
+  }
+  while(right.length) {
+    result.push(right.shift());
+  }
+  return result;
+}
+
+function quickSort(nums) {
+  if (nums.length < 2) {
+    return nums;
+  }
+  const pivot = nums.pop();
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] < pivot ? left.push(nums[i]) : right.push(nums[i]);
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
